a3e7013414cdfac583a28513e1dd33804519d50c
nfianK <85173487+nfianK@users.noreply.github.com>
2022-02-09 17:19:34 -0300

    Create Insersion.sql

---
+++ Insersion.sql
@@ -0,0 +1,2 @@
+use spinetta;
+insert into banda values (1, "Almendra", 1968, 12, 3), (default, "Pescado Rabioso", 1971, 2, 3), (default, "Spinetta Jade", 1980, 5, 4), (default, "Invisible", 1973, 4, 3), (default, "Spinetta y los Socios del Desierto", 1994, 5, 3);
